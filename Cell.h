534cc4371d470a0e31c4a056cc5d5d34400c96aa
HosseinAbedZ <hossein.abedz3937@gmail.com>
2023-04-08 03:39:28 +0430

    created reverse mutation

--- Cell.h
+++ Cell.h
@@ -29,7 +29,7 @@ public:
         this->DNA[0] = _DNA;
         this->DNA[1] = _DNA2;
     }
-    
+
 
     // saakht DNA az RNA
     void RNAtoDNA()
@@ -160,6 +160,91 @@ public:
                     mutantDNA[0] = BDNA + _s2 + ADNA;
 
                 }
+    }
+
+
+    // jahesh maakoos
+    void reverseMutation(string s1)
+    {
+        //reverse for RNA:
+            string BRNA = "";
+            // BRNA : zirreshte ghabl az s1
+            for(int i = 0; i < RNA.find(s1); i++)
+            {
+                BRNA += RNA[i];
+            } 
+            string ARNA = "";
+            // ARNA : zirreshte baad az s1
+            for(int i = BRNA.size() + s1.size(); i < RNA.size(); i++)
+            {
+                ARNA += RNA[i];
+            }
+
+            reverse(s1.begin(), s1.end());
+            // reverse mutation RNA:
+            string rRNA = BRNA + s1 + ARNA;
+
+
+        //reverse for DNA:
+            if(DNA[0].find(s1) < DNA[1].find(s1)) // s1 dar reshte avval peyda shode
+            {
+                string BDNA = "";
+                // BDNA : zirreshte ghabl az s1
+                for(int i = 0; i < DNA[0].find(s1); i++)
+                {
+                    BDNA += DNA[0][i];
+                } 
+
+                string ADNA = "";
+                // ADNA : zirreshte baad az s1
+                for(int i = BDNA.size() + s1.size(); i < DNA[0].size(); i++)
+                {
+                    ADNA += DNA[0][i];
+                }
+
+                reverse(s1.begin(), s1.end());
+                string rDNA[2];
+                rDNA[0] = BDNA + s1 + ADNA;
+                // mokamel dar reshte digar:
+                    string _s1 = "";
+                    for (int i = 0; i < s1.size(); i++)
+                    {
+                        _s1 += Complementary(s1[i]);
+                    }
+                rDNA[1] = BDNA + _s1 + ADNA;
+
+            }
+
+            else // s1 dar reshte dovom peyda shode
+            {
+                string BDNA = "";
+                // BDNA : zirreshte ghabl az s1
+                for(int i = 0; i < DNA[1].find(s1); i++)
+                {
+                    BDNA += DNA[1][i];
+                } 
+
+                string ADNA = "";
+                // ADNA : zirreshte baad az s1
+                for(int i = BDNA.size() + s1.size(); i < DNA[1].size(); i++)
+                {
+                    ADNA += DNA[1][i];
+                }
+
+                reverse(s1.begin(), s1.end());
+                string rDNA[2];
+                rDNA[1] = BDNA + s1 + ADNA;
+                // mokamel dar reshte digar:
+                    string _s1 = "";
+                    for (int i = 0; i < s1.size(); i++)
+                    {
+                        _s1 += Complementary(s1[i]);
+                    }
+                rDNA[0] = BDNA + _s1 + ADNA;
+
+            }
+
+
     }
 
 
