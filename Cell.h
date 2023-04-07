15fc458abc883abf6b078a140c652a8624ca722d
HosseinAbedZ <hossein.abedz3937@gmail.com>
2023-04-07 23:12:56 +0430

    init: project init

---
+++ Cell.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Genome
+{
+    private:
+        string RNA;
+        string DNA[2];
+    public:
+        Genome() {}
+        
+};
