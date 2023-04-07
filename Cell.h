a777c11937df5181c9086bd9ad04f512ca1d834f
HosseinAbedZ <hossein.abedz3937@gmail.com>
2023-04-08 00:42:07 +0430

    create RNAtoDNA

--- Cell.h
+++ Cell.h
@@ -6,6 +6,20 @@
 
 using namespace std;
 
+// mokamelsaz
+string Complementary(string str)
+{
+    for(int i = 0; i < str.size(); i++)
+    {
+        if( str[i] == "A") { str[i] = "T";}
+        else if ( str[i] == "T") { str[i] = "A";}
+        else if ( str[i] == "C") { str[i] = "G";}
+        else if ( str[i] == "G") { str[i] = "C";}
+    }
+
+    return str;
+}
+
 class Genome
 {
     private:
@@ -13,5 +27,16 @@ class Genome
         string DNA[2];
     public:
         Genome() {}
-        
+        Genome(string _RNA, string _DNA, string _DNA2)
+        {
+            this->RNA = _RNA;
+            this->DNA[0] = _DNA;
+            this->DNA[1] = _DNA2;
+        }
+        // saakht DNA az RNA
+        void RNAtoDNA()
+        {
+            cout << "DNA from RNA : " << endl << RNA << endl;
+            cout << Complementary(RNA) << endl;
+        }
 };
