e7c2f4da3c429bb91de0e6bd98d9e29633f63b4b
alirezaniazi <130310530+alirezaniazi@users.noreply.github.com>
2023-04-17 14:32:26 -0700

    Update main.cpp
    The menu for the animal needs to be changed

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main()
      for (int i = 0;; i++)
      {
           cout << "1. Make change on DNA and RNA" << endl;
-          cout << "2. Make change on chromosomes" << endl;
+          cout << "2. Examination or changes in chromosomes" << endl;
           int change;
           cin >> change;
 
@@ -114,120 +114,207 @@ int main()
 
           else if (change == 2) // make change on chromosomes:
           {
-               cout << "How many chromosomes do you want?:" << endl;
-               int chro;
-               cin >> chro;
-               vector<Genome> _chro;
-               for (int i = 0; i < chro; i++)
+               cout << " 1) make change on chromosomes" << endl;
+               cout << " 2) Chromosome examination" << endl;
+
+               int oneORtwo;
+               cin >> oneORtwo;
+
+               if (oneORtwo == 1)
                {
-                    cout << "Enter the first string of DNA " << i + 1 << "  :";
-                    string S1;
-                    cin >> S1;
-                    cout << "Enter the second string of DNA " << i + 1 << " :";
-                    string S2;
-                    cin >> S2;
-                    Genome ch(S1, S2);
-                    _chro.push_back(ch);
+                    cout << "How many chromosomes do you want?:" << endl;
+                    int chro;
+                    cin >> chro;
+                    vector<Genome> _chro;
+                    for (int i = 0; i < chro; i++)
+                    {
+                         cout << "Enter the first string of DNA " << i + 1 << "  :";
+                         string S1;
+                         cin >> S1;
+                         cout << "Enter the second string of DNA " << i + 1 << " :";
+                         string S2;
+                         cin >> S2;
+                         Genome ch(S1, S2);
+                         _chro.push_back(ch);
+                    }
+                    Cell C;
+                    C.chromosomes_storage(_chro);
+
+                    cout << "----------------------------------" << endl;
+                    cout << "|                                |" << endl;
+                    cout << "| 1) Make a small mutation       |" << endl;
+                    cout << "| 2) Make a long mutation        |" << endl;
+                    cout << "| 3) Make a reverse mutation     |" << endl;
+                    cout << "| 4) Find all the palindromes    |" << endl;
+                    cout << "| 5) Exit                        |" << endl;
+                    cout << "|                                |" << endl;
+                    cout << "----------------------------------" << endl;
+
+                    int choice2;
+                    cin >> choice2;
+
+                    if (choice2 == 1)
+                    {
+                         cout << "First, enter the desired nucleotide, then the replacement nucleotide, and enter the change number and chromosome number, respectively " << endl;
+
+                         char nuc1, nuc2;
+                         int num1, num2;
+
+                         cout << "First nucleotide : ";
+                         cin >> nuc1;
+
+                         cout << "Second nucleotide : ";
+                         cin >> nuc2;
+
+                         cout << "Number of changes : ";
+                         cin >> num1;
+
+                         cout << "number of chromosome : ";
+                         cin >> num2;
+
+                         C.mutation(nuc1, nuc2, num1, num2);
+
+                         cout << "Short Mutant DNA : " << endl;
+                         cout << C.getChromosomes()[num2 - 1].getDNA1() << endl
+                              << C.getChromosomes()[num2 - 1].getDNA2() << endl;
+                    }
+                    else if (choice2 == 2)
+                    {
+                         cout << "For long mutation \n First, enter the first substring and its chromosome number, then the second substring and its chromosome number " << endl;
+
+                         string substr1, substr2;
+                         int num1, num2;
+
+                         cout << "desired substring : ";
+                         cin >> substr1;
+
+                         cout << "number of first chromosome : ";
+                         cin >> num1;
+
+                         cout << "alternative substring : ";
+                         cin >> substr2;
+
+                         cout << "number of second chromosome : ";
+                         cin >> num2;
+
+                         C.longMutation(substr1, num1, substr2, num2);
+
+                         cout << "long mutant DNA in chromosome " << num1 << " : " << endl;
+                         cout << C.getChromosomes()[num1 - 1].getDNA1() << endl
+                              << C.getChromosomes()[num1 - 1].getDNA2() << endl
+                              << endl;
+                         cout << "long mutant DNA in chromosome " << num2 << " : " << endl;
+                         cout << C.getChromosomes()[num2 - 1].getDNA1() << endl
+                              << C.getChromosomes()[num2 - 1].getDNA2() << endl
+                              << endl;
+                    }
+                    else if (choice2 == 3)
+                    {
+                         cout << "For reverse mutation, enter the desired string and its chromosome number" << endl;
+
+                         cout << "enter your desired substring to reverse mutation : ";
+                         string s1;
+                         cin >> s1;
+
+                         cout << "enter number of chromosome : ";
+                         int n;
+                         cin >> n;
+
+                         C.reverseMutation(s1, n);
+
+                         cout << "Reverse Mutant DNA : " << endl;
+                         cout << C.getChromosomes()[n - 1].getDNA1() << endl
+                              << C.getChromosomes()[n - 1].getDNA2() << endl;
+                    }
+                    else if (choice2 == 4)
+                    {
+                         cout << "To find a palindrome in a chromosome, enter the chromosome number : " << endl;
+                         int n;
+                         cin >> n;
+                         C.PrintAllPalindromes(n);
+                    }
+                    else if (choice2 == 5)
+                    {
+                         break;
+                    }
                }
-               Cell C;
-               C.chromosomes_storage(_chro);
 
-               cout << "----------------------------------" << endl;
-               cout << "|                                |" << endl;
-               cout << "| 1) Make a small mutation       |" << endl;
-               cout << "| 2) Make a long mutation        |" << endl;
-               cout << "| 3) Make a reverse mutation     |" << endl;
-               cout << "| 4) Find all the palindromes    |" << endl;
-               cout << "| 5) Exit                        |" << endl;
-               cout << "|                                |" << endl;
-               cout << "----------------------------------" << endl;
-
-               int choice2;
-               cin >> choice2;
-
-               if (choice2 == 1)
+               else if (oneORtwo == 2)
                {
-                    cout << "First, enter the desired nucleotide, then the replacement nucleotide, and enter the change number and chromosome number, respectively " << endl;
+                    // first animal
+                    cout << "How many chromosomes do you want for the first animal?:" << endl;
+                    int chro;
+                    cin >> chro;
+                    vector<Genome> _chro;
+                    for (int i = 0; i < chro; i++)
+                    {
+                         cout << "Enter the first string of DNA " << i + 1 << "  :";
+                         string S1;
+                         cin >> S1;
+                         cout << "Enter the second string of DNA " << i + 1 << " :";
+                         string S2;
+                         cin >> S2;
+                         Genome ch(S1, S2);
+                         _chro.push_back(ch);
+                    }
 
-                    char nuc1, nuc2;
-                    int num1, num2;
+                    Cell C;
+                    C.chromosomes_storage(_chro);
 
-                    cout << "First nucleotide : ";
-                    cin >> nuc1;
+                    // second animal
+                    cout << "How many chromosomes do you want for the second animal?:" << endl;
+                    int chro2;
+                    cin >> chro2;
+                    vector<Genome> _chro2;
+                    for (int i = 0; i < chro2; i++)
+                    {
+                         cout << "Enter the first string of DNA " << i + 1 << "  :";
+                         string _S1;
+                         cin >> _S1;
+                         cout << "Enter the second string of DNA " << i + 1 << " :";
+                         string _S2;
+                         cin >> _S2;
+                         Genome ch2(_S1, _S2);
+                         _chro2.push_back(ch2);
+                    }
 
-                    cout << "Second nucleotide : ";
-                    cin >> nuc2;
+                    cout << "------------------------------------------" << endl;
+                    cout << "|                                        |" << endl;
+                    cout << "| 1) Percentage of genetic similarity    |" << endl;
+                    cout << "| 2) Identification of similar species   |" << endl;
+                    cout << "| 3) Asexual reproduction                |" << endl;
+                    cout << "| 4) sexual reproduction                 |" << endl;
+                    cout << "| 5) Cell death                          |" << endl;
+                    cout << "| 6) Exit                                |" << endl;
+                    cout << "|                                        |" << endl;
+                    cout << "------------------------------------------" << endl;
 
-                    cout << "Number of changes : ";
-                    cin >> num1;
+                    int choice3;
+                    cin >> choice3;
 
-                    cout << "number of chromosome : ";
-                    cin >> num2;
+                    Animal A;
 
-                    C.mutation(nuc1, nuc2, num1, num2);
-
-                    cout << "Short Mutant DNA : " << endl;
-                    cout << C.getChromosomes()[num2 - 1].getDNA1() << endl
-                         << C.getChromosomes()[num2 - 1].getDNA2() << endl;
-               }
-               else if (choice2 == 2)
-               {
-                    cout << "For long mutation \n First, enter the first substring and its chromosome number, then the second substring and its chromosome number " << endl;
-
-                    string substr1, substr2;
-                    int num1, num2;
-
-                    cout << "desired substring : ";
-                    cin >> substr1;
-
-                    cout << "number of first chromosome : ";
-                    cin >> num1;
-
-                    cout << "alternative substring : ";
-                    cin >> substr2;
-
-                    cout << "number of second chromosome : ";
-                    cin >> num2;
-
-                    C.longMutation(substr1, num1, substr2, num2);
-
-                    cout << "long mutant DNA in chromosome " << num1 << " : " << endl;
-                    cout << C.getChromosomes()[num1 - 1].getDNA1() << endl
-                         << C.getChromosomes()[num1 - 1].getDNA2() << endl
-                         << endl;
-                    cout << "long mutant DNA in chromosome " << num2 << " : " << endl;
-                    cout << C.getChromosomes()[num2 - 1].getDNA1() << endl
-                         << C.getChromosomes()[num2 - 1].getDNA2() << endl
-                         << endl;
-               }
-               else if (choice2 == 3)
-               {
-                    cout << "For reverse mutation, enter the desired string and its chromosome number" << endl;
-
-                    cout << "enter your desired substring to reverse mutation : ";
-                    string s1;
-                    cin >> s1;
-
-                    cout << "enter number of chromosome : ";
-                    int n;
-                    cin >> n;
-
-                    C.reverseMutation(s1, n);
-
-                    cout << "Reverse Mutant DNA : " << endl;
-                    cout << C.getChromosomes()[n - 1].getDNA1() << endl
-                         << C.getChromosomes()[n - 1].getDNA2() << endl;
-               }
-               else if (choice2 == 4)
-               {
-                    cout << "To find a palindrome in a chromosome, enter the chromosome number : " << endl;
-                    int n;
-                    cin >> n;
-                    C.PrintAllPalindromes(n);
-               }
-               else if (choice2 == 5)
-               {
-                    break;
+                    if (choice3 == 1)
+                    {
+                         A.SimilarityPercentageOfTwoAnimal(A.getChromosomes(), _chro2);
+                    }
+                    if (choice3 == 2)
+                    {
+                    }
+                    if (choice3 == 3)
+                    {
+                         A.AsexualReproduction();
+                    }
+                    if (choice3 == 4)
+                    {
+                    }
+                    if (choice3 == 5)
+                    {
+                    }
+                    if (choice3 == 6)
+                    {
+                         break;
+                    }
                }
           }
      }
